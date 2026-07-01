f09bef6be88d35d63df824ae5bfe09434513b744
Andrés Miranda <amiranda20132013@gmail.com>
2026-07-01 10:31:45 -0500

    Estructura base de la app Futurama
    Configuración inicial para el proyecto.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="app">
+      <header className="home-header">
+        <h1>Futurama</h1>
+      </header>
+
+      <main className="home-content">
+        <h2>Personajes</h2>
+      </main>
+    </div>
+  )
+}
+
+export default Home
